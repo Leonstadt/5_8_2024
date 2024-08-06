6e6a2cf924e77fd9a02f73d901b8915ed26f5a52
Leonstadt <alfredo.ordonez@tecnologicoliceocristiano.edu.ec>
2024-08-05 20:26:21 -0500

    Add files via upload

---
+++ fecha.py
@@ -0,0 +1,14 @@
+meses={"enero":1, "febrero":2, "marzo":3,  
+"abril":4, "mayo":5, "junio":6,
+ "julio":7, "agosto":8, "septiembre":9,
+ "octubre":10, "noviembre": 11, "diciembre":12 }
+fecha=input("Introduce una fecha: ")
+formato1=fecha.replace(",", "")
+formato2=formato1.split(" ")
+print(formato2)
+dia=int(formato2[0])
+mes=(formato2[1])
+año=int(formato2[2])
+numeromes=meses[mes]
+print(f"{dia} {numeromes} {año}")
+
